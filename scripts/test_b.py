d89dfbfd2716b9fbadde4a16dd2043605b52cc8c
Alex Baldwin <alewin@noc.ac.uk>
2025-11-20 11:46:18 +0000

    Refactor

--- scripts/test_b.py
+++ scripts/test_b.py
@@ -14,7 +14,7 @@ drs = deposit.get_prefixed_udts("udt1_2dc621accf3d_c224a43f022373c2_00006838f56e
 print("Data records found:" + str(len(drs)))
 
 import cv2
-for i in range(0,25):
+for i in range(0,8):
     gray_image = cv2.cvtColor(deposit.get_data_record(drs[i]).as_array(), cv2.COLOR_GRAY2BGR)
     cv2.imshow("Image " + str(i) + " from deposit", gray_image)
 cv2.waitKey(10000)
